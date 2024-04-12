ae844cb9d3da3488cc73fedb991f230251f59eb8
therealartti <107062680+therealartti@users.noreply.github.com>
2024-04-12 13:49:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cyber Security Project I
+
+This project was created as an assignment for Helsinki university's Cyber Security Base 2024 course (Project I). It follows Django's [7-part tutorial](https://docs.djangoproject.com/en/3.1/intro/tutorial01/), while incorporating 5 vulnerabilities:
+- Security Misconfiguration
+- Security Logging and Monitoring Failures
+- Cryptographic Failures
+- Broken Access Control
+- Injection
+
+More on that can be found in report.md.
+
+### Installation Instructions
+
+1. Run `python manage.py migrate` to create the polls models.
+
+2. Run `python manage.py runserver` to start the development server.
+
+3. Visit http://127.0.0.1:8000/admin/ to create a poll.
+
+4. Visit http://127.0.0.1:8000/polls/ to participate in the poll.
+
+### Ready-made account:
+- test:tested123
---
+++ mysite/settings.py
@@ -0,0 +1,103 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'password'
+# A safer secret key should be used in production (flaw 1)
+'''
+SECRET_KEY = 'beflvb4w4tlnTfI'
+'''
+
+DEBUG = True
+# Debug mode should be off in production (flaw 2)
+'''
+DEBUG = False
+'''
+
+ALLOWED_HOSTS = []
+
+
+INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'mysite.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'mysite.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+STATIC_URL = '/static/'
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+# Secure cookies should be enabled in production (flaw 3)
+'''
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+'''
